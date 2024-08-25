0e9b0bb4e8881227db67311c3590d9d65616ac0a
Yana Kukhta <kyv@b2bit.pro>
2024-08-25 13:47:59 +0300

    save locale changes

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
     plugins: [
         react(),
         keycloakify({
+            extraThemeProperties: ["locales=ru,en"],
             accountThemeImplementation: "none"
         })
     ]
