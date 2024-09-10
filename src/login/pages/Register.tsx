42cf52c4360ac361f811d81393d19eb88142abff
Yana Kukhta <kyv@b2bit.pro>
2024-09-10 15:50:05 +0300

    delete pattern

--- src/login/pages/Register.tsx
+++ src/login/pages/Register.tsx
@@ -107,7 +107,6 @@ export default function Register(props: PageProps<Extract<KcContext, { pageId: "
               <div className="relative z-0 w-[500px] mb-5 group">
                 <input
                   type="tel"
-                  pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
                   name="floating_phone"
                   id="floating_phone"
                   className="block py-2.5 px-0 w-[500px] text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
