4ff6512c3f568dc08d325169b365cf0485a3a3cd
Yana Kukhta <kyv@b2bit.pro>
2024-09-10 15:56:21 +0300

    add login binding

--- src/login/pages/Login/index.tsx
+++ src/login/pages/Login/index.tsx
@@ -30,7 +30,11 @@ export default function Login(
                 <LangSelect {...props} />
               </div>
 
-              <form className="space-y-4 md:space-y-6" action="#">
+              <form
+                className="space-y-4 md:space-y-6"
+                action={url.loginAction}
+                method="post"
+              >
                 <div>
                   <label
                     htmlFor="email"
@@ -41,6 +45,8 @@ export default function Login(
                   <input
                     className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                     required
+                    id="username"
+                    name="username"
                   />
                 </div>
                 <div>
