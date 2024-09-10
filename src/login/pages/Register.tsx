4610687e19d4e1381924e54a604afe093c6bf183
Yana Kukhta <kyv@b2bit.pro>
2024-09-10 16:29:28 +0300

    edit register form

--- src/login/pages/Register.tsx
+++ src/login/pages/Register.tsx
@@ -19,14 +19,14 @@ export default function Register(props: PageProps<Extract<KcContext, { pageId: "
           <LangSelect {...props} />
         </div>
 
-        <form>
+        <form action={url.registrationAction} method="post">
           <div className="flex justify-between w-[1200px] mx-auto">
             <div>
               <div className="relative w-[500px] z-0 mb-5 group">
                 <input
-                  type="email"
-                  name="floating_email"
-                  id="floating_email"
+                  type="username"
+                  name="username"
+                  id="register_username"
                   className="block py-2.5 px-0 w-[500px] text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                   placeholder=" "
                   required
@@ -41,8 +41,8 @@ export default function Register(props: PageProps<Extract<KcContext, { pageId: "
               <div className="relative z-0 w-[500px] mb-5 group">
                 <input
                   type="password"
-                  name="floating_password"
-                  id="floating_password"
+                  name="password"
+                  id="register_password"
                   className="block py-2.5 px-0 w-[500px] text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                   placeholder=" "
                   required
@@ -54,7 +54,7 @@ export default function Register(props: PageProps<Extract<KcContext, { pageId: "
                   {msgStr("password")}
                 </label>
               </div>
-              <div className="relative z-0 w-[500px] mb-5 group">
+              {/* <div className="relative z-0 w-[500px] mb-5 group">
                 <input
                   type="password"
                   name="repeat_password"
@@ -69,20 +69,20 @@ export default function Register(props: PageProps<Extract<KcContext, { pageId: "
                 >
                   {msgStr("confirmPassword")}
                 </label>
-              </div>
+              </div> */}
             </div>
             <div>
               <div className="relative z-0 w-[500px] mb-5 group">
                 <input
                   type="text"
-                  name="floating_first_name"
-                  id="floating_first_name"
+                  name="firstName"
+                  id="firstName"
                   className="block py-2.5 px-0 w-[500px] text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                   placeholder=" "
                   required
                 />
                 <label
-                  htmlFor="floating_first_name"
+                  htmlFor="firstName"
                   className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                 >
                   {msgStr("firstName")}
@@ -91,46 +91,46 @@ export default function Register(props: PageProps<Extract<KcContext, { pageId: "
               <div className="relative z-0 w-[500px] mb-5 group">
                 <input
                   type="text"
-                  name="floating_last_name"
+                  name="lastName"
                   id="floating_last_name"
                   className="block py-2.5 px-0 w-[500px] text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                   placeholder=" "
                   required
                 />
                 <label
-                  htmlFor="floating_last_name"
+                  htmlFor="lastName"
                   className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                 >
                   {msgStr("lastName")}
                 </label>
               </div>
-              <div className="relative z-0 w-[500px] mb-5 group">
+              {/* <div className="relative z-0 w-[500px] mb-5 group">
                 <input
                   type="tel"
-                  name="floating_phone"
-                  id="floating_phone"
+                  name="phone"
+                  id="phone"
                   className="block py-2.5 px-0 w-[500px] text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                   placeholder=" "
                   required
                 />
                 <label
-                  htmlFor="floating_phone"
+                  htmlFor="phone"
                   className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                 >
                   {msgStr("phoneNumber")}
                 </label>
-              </div>
+              </div> */}
               <div className="relative z-0 w-[500px] mb-5 group">
                 <input
                   type="text"
-                  name="floating_company"
-                  id="floating_company"
+                  name="company"
+                  id="company"
                   className="block py-2.5 px-0 w-[500px] text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                   placeholder=" "
                   required
                 />
                 <label
-                  htmlFor="floating_company"
+                  htmlFor="company"
                   className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                 >
                   {msgStr("company")}
